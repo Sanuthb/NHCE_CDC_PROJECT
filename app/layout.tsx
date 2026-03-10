ce43cc50862a4dfc0800d761afd746428bb4bda4
Sanuth Bibin.B <165442353+Sanuthb@users.noreply.github.com>
2026-03-10 18:36:01 +0530

    Add RootLayout component with metadata and styles
    Implement root layout with metadata and font styles.

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Providers } from "@/components/pageComponents/Providers";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "NHCE CDC Exam Portal",
+  description: "Secure and efficient examination platform for student assessments",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Providers>{children}</Providers>
+        <Analytics />
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
